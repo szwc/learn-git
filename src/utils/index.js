b9953e201a09e3ef29c1f8ee79b08798f2c92610
carrot <songzw@broada.com>
2023-08-11 14:50:33 +0800

    tt

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+let a = 10;
+
+test();
+
+function test() {
+  console.log("111");
+  console.log("111");
+  console.log(a);
+}
--- utils/index.js
+++
@@ -1,8 +0,0 @@
- var a = 10
-
- test()
-
- function test(){
-    var a = 1
-    console.log('111');
- }
